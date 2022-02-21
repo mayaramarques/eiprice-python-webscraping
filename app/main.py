21e0ae45cf4370e50c2916728528fe2903d3d11c
Denis Lopes <dlopes@dataguard.de>
2022-02-20 22:35:16 -0300

    criado componente para conexão de api com fastapi

--- app/main.py
+++ app/main.py
@@ -1,36 +1,36 @@
+from threading import Thread
+
 from fastapi import FastAPI
-from ..scripts import ShopperWebScraping
+
+from app.model import Assortment
+from scripts.ShopperWebScraping import ShopperWebScraping
+from app.component.AssortmentComponent import AssortmentComponent
 
 app = FastAPI()
 
-@app.post("/scripts/shooper/start")
-def start_shopper():
-  script = ShopperWebScraping.Coleta()
 
-@app.get("/scripts/shopper/status")
-def check_shopper_status():
-  return
+@app.post("/scripts/shopper/start")
+def start_shopper():
+    process = ShopperWebScraping()
+    process.start_collect()
+
 
 @app.get("/api/assortment")
-def get_assortment():
-  return
+def get_assortments() -> dict[Assortment]:
+    component = AssortmentComponent()
+    result = component.get_assortments()
+    return result
+
 
 @app.get("/api/assortment/product/{product_name}")
-def get_assortment_by_procuct_name(product_name: str):
-  return
+def get_assortment_by_product_name(product_name: str) -> dict[Assortment]:
+    component = AssortmentComponent()
+    result = component.get_assortment_by_product_name(product_name)
+    return result
+
 
 @app.get("/api/assortment/department/{department_name}")
-def get_assortment_by_department_name(department_name: str):
-  return
-
-@app.get("/api/seller")
-def get_seller():
-  return
-
-@app.get("/api/seller/product/{product_name}")
-def get_seller_by_procuct_name(product_name: str):
-  return
-
-@app.get("/api/seller/department/{department_name}")
-def get_seller_by_department_name(department_name: str):
-  return
+def get_assortment_by_department_name(department_name: str) -> dict[Assortment]:
+    component = AssortmentComponent()
+    result = component.get_assortment_by_department_name(department_name)
+    return result
